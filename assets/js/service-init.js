18768974ccef1301da6c2f0f68fb4a36fcc02469
Yuttana <notebig@gmail.com>
2026-02-13 01:05:00 +0700

    set  template

---
+++ assets/js/service-init.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  includeHTML('#header', '/partials/header-services.html');
+  includeHTML('#footer', '/partials/footer-services.html');
+
+  const reveals = document.querySelectorAll(".reveal");
+
+  if(reveals.length){
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) entry.target.classList.add("active");
+      });
+    }, { threshold: 0.2 });
+
+    reveals.forEach(r => observer.observe(r));
+  }
+
+});
